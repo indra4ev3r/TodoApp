3a201096ee2b0168ee80cd9b7fe1a619014dc4a2
unknown <indrateja.pachipala@gmail.com>
2020-03-19 02:02:43 -0500

    todo app

---
+++ src/data.js
@@ -0,0 +1,16 @@
+let todosData = [
+    {
+        text: 'Learn about React',
+        isCompleted: false
+      },
+      {
+        text: 'Meet React',
+        isCompleted: false
+      },
+      {
+        text: 'Build React',
+        isCompleted: false
+      }
+];
+
+export default todosData;
